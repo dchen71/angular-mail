057b0fe3060e09aba61230239ff2d719c81fe028
Dan <dchen12812@gmail.com>
2015-08-16 16:32:35 -0700

    added directives

--- js/app.js
+++ js/app.js
@@ -2,14 +2,12 @@ angular.module('EmailApp', [
   'ngRoute'
 ]).config(function ( $routeProvider ) {
   'use strict';
-  // configure urls
   $routeProvider
-    // inbox route
     .when('/inbox', {
       templateUrl: 'views/inbox.html',
-      controller: 'InboxCtrl', // map js to html scope
+      controller: 'InboxCtrl'
     })
-    .otherwise({ // default
+    .otherwise({
       redirectTo: '/inbox'
     });
 });
